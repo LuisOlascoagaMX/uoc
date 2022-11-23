113c230181c544711745ce1d83daadccadd6fde0
Luis Olascoaga <luis.olascoaga.dominguez@gmail.com>
2022-11-23 00:08:57 -0600

    data minning - pec 2

---
+++ mineria_datos/pec2/PEC2.R
@@ -0,0 +1,8 @@
+#Obtenemos el dataset refrente a los halcones.
+if (!require('Stat2Data')) install.packages('Stat2Data')
+library(Stat2Data)
+data("Hawks")
+summary(Hawks)
+
+# Dataset hawks -----------------------------------------------------------
+
